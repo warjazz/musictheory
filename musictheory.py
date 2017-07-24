3282c715e78a50c50d8d5e547360e9d0bf77ae23
Warren Fletcher <w.fletcherZA@gmail.com>
2017-07-24 22:51:48 +0200

    Initial Commit

---
+++ musictheory.py
@@ -0,0 +1,7 @@
+notes = ['A','A#', 'B','C', 'C#','D','D#','E','F','F#','G','G#']
+modOrder = ['F','C','G','D','A','E','B']
+keys = ['G','D','A','E','B','F','C']
+modeNames = ['Ionian', 'Dorian','Phyrgian','Lydian','Mixolydian','Aeolian','Locrian']
+majorIntervals = ['T','T','S','T','T','T','S']
+intervals = ['Root','Minor 2nd','Major 2nd','Minor 3rd','Major 3rd','Perfect 4th','Diminshed 5th', 'Perfect 5th', 'Augmented 5th', 'Major 6th','Minor 7th', 'Major 7th']
+
